4e204ee6bd16d7e324ffe6963d06b22dd5a3cd07
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-08-15 16:12:59 +0200

    Solve lint warnings

--- src/components/App/App.types.ts
+++ src/components/App/App.types.ts
@@ -1,30 +1,21 @@
-import { BigNumber, ethers } from "ethers";
-import { AnyAction, Dispatch } from "redux";
-import {
-  ConnectWalletRequestAction,
-  UpdateBalanceAction,
-} from "../../modules/wallet/actions";
+import { BigNumber, ethers } from 'ethers'
+import { AnyAction, Dispatch } from 'redux'
+import { ConnectWalletRequestAction } from '../../modules/wallet/actions'
 
 export type Props = {
-  provider: ethers.providers.Web3Provider | null;
-  address: string;
-  symbol: string;
-  balance: BigNumber;
-  isConnected: boolean;
-  isConnecting: boolean;
-  error: string | null;
-  onConnect: () => void;
-};
+  provider: ethers.providers.Web3Provider | null
+  address: string
+  symbol: string
+  balance: BigNumber
+  isConnected: boolean
+  isConnecting: boolean
+  error: string | null
+  onConnect: () => void
+}
 
 export type MapStateProps = Pick<
   Props,
-  | "provider"
-  | "address"
-  | "symbol"
-  | "balance"
-  | "isConnected"
-  | "isConnecting"
-  | "error"
->;
-export type MapDispatchProps = Pick<Props, "onConnect">;
-export type MapDispatch = Dispatch<ConnectWalletRequestAction | AnyAction>;
+  'provider' | 'address' | 'symbol' | 'balance' | 'isConnected' | 'isConnecting' | 'error'
+>
+export type MapDispatchProps = Pick<Props, 'onConnect'>
+export type MapDispatch = Dispatch<ConnectWalletRequestAction | AnyAction>
--- src/components/modals/TransferModal/TransferModal.types.ts
+++ src/components/modals/TransferModal/TransferModal.types.ts
@@ -1,9 +1,7 @@
-import { BigNumber } from "ethers";
-
 export type Props = {
-  dummyBalance: string;
-  dummySymbol: string;
-  showModal?: boolean;
-  onClose?: () => void;
-  onSubmit: (address: string, amount: string) => Promise<void>;
-};
+  dummyBalance: string
+  dummySymbol: string
+  showModal?: boolean
+  onClose?: () => void
+  onSubmit: (address: string, amount: string) => Promise<void>
+}
